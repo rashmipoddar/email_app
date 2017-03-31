5af321dc5430ff544ca72c92fc5c2031f4744326
Rashmi <poddar.rashmi@gmail.com>
2017-03-31 19:19:57 +0530

    Created form for entering email

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-index.js
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var router = express.Router();
+
+var text = 'Hello world from \n\n' + req.body.name;
+
+var mailOptions = {
+	from: 'rashmi.poddar2017@gmail.com',
+	to: 'rashmi.poddar@appbrowzer.com',
+	subject: 'Hello',
+	
+}
+app.use('/email', router);
+router.post('/', sendEmail);
+
+
+
+
+
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function() {
+	console.log('Listening on port 3000');
+});
+
+function sendEmail(req, res) {
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'rashmi.poddar2017@gmail.com',
+			pass: 'rashmipoddar2017'
+		}
+	});
+}
